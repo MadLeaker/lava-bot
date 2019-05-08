1f3d22e63e394b1b3b64f49ef92278b2dc254c94
MadLeaker <43907203+MadLeaker@users.noreply.github.com>
2019-05-08 15:29:55 +0300

    Update main.js

--- main.js
+++ main.js
@@ -68,8 +68,7 @@ function getPatchNotes() {
 function newTeaser(day,interval) {
     T.get('statuses/user_timeline',{screen_name: "FortniteGame",count:50,tweet_mode:"extended"},function(err,tweet,resp) {
     tweet.forEach(async tw => {
-        console.log(tw.full_text);
-        if(tw.entities.media && tw.full_text.includes("#FortniteSeason9") && tw.created_at.includes(day))
+              if(tw.entities.media && tw.full_text.includes("#FortniteSeason9") && tw.created_at.includes(day))
         {
             console.log("New Teaser Available");
           await downloadFile(tw.entities.media[0].media_url_https,"Teaser3.png");
