c8099f81aabc82e51bb99b95236a42309cd1176d
MadLeaker <43907203+MadLeaker@users.noreply.github.com>
2019-05-08 13:53:08 +0300

    New Things

--- main.js
+++ main.js
@@ -1,6 +1,41 @@
 const Discord = require("discord.js")
 const request = require("request")
 const Client = new Discord.Client()
+const twit = require("twit")
+const Path = require('path')
+const fs = require('fs')
+const axios = require("axios")
+
+const thingys = {
+    consumer_key:         process.env.cons_key,
+   consumer_secret:      process.env.cons_sec,
+   access_token:         process.env.acc_token,
+   access_token_secret:  process.env.acc_token_sec,
+   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
+   strictSSL:            true,     // optional - requires SSL certificates to be valid. 
+ }
+ 
+ const T = new twit(thingys);
+
+ async function downloadFile (url,name){  
+    const path = Path.resolve(__dirname, name)
+    const writer = fs.createWriteStream(path)
+  
+    const response = await axios({
+      url,
+      method: 'GET',
+      responseType: 'stream'
+    })
+  
+    response.data.pipe(writer)
+  
+    return new Promise((resolve, reject) => {
+      writer.on('finish', resolve)
+      writer.on('error', reject)
+    })
+  }
+
+
 
 
 function getPatchNotes() {
@@ -30,11 +65,34 @@ function getPatchNotes() {
 }
 
 
+function newTeaser(day) {
+    T.get('statuses/user_timeline',{screen_name: "FortniteGame",count:50,tweet_mode:"extended"},function(err,tweet,resp) {
+    tweet.forEach(async tw => {
+        if(tw.entities.media && tw.full_text.includes("#FortniteSeason9") && tw.created_at.includes(day))
+        {
+          await downloadFile(tw.entities.media[0].media_url_https,"Teaser3.png");
+          const channel = Client.channels.get("574297690943520789")
+          channel.fetchMessages({limit: 1}).then(messages => {
+              messages.forEach(msg => {
+                    if(!msg.content.includes("3rd Teaser")) {
+                        channel.send("3rd Teaser Is Out!",{files: [
+                            "./Teaser3.png"
+                        ]})
+                    }   
+              })
+          })
+        }
+        
+    })
+  }) 
+  }
+
 
 
 Client.once("ready",() => {
 
     setInterval(getPatchNotes,1000);
+    setInterval(function() {newTeaser("Wed")},1000)
 })
 
 Client.on("guildMemberAdd",function(member) {
